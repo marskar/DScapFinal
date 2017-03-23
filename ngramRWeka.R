20beb8e7fa1d6fadc9ffc62c171d0512cf087fda
marskar <marskar@gmail.com>
2017-03-23 15:44:18 -0400

    Finalized data prep - saved trimmed ngram lists

---
+++ ngramRWeka.R
@@ -0,0 +1,17 @@
+install.packages("RWeka")
+install.packages("tm")
+library(tm)
+library(RWeka)
+dat1<-iconv(dat, "UTF-8", "ASCII", sub="")
+corp <- VCorpus(VectorSource(dat1))
+corp <- tm_map(corp, tolower)
+corp <- tm_map(corp, removePunctuation)
+corp <- tm_map(corp, removeNumbers)
+corp <- tm_map(corp, stripWhitespace)
+corp <- tm_map(corp, PlainTextDocument)
+
+bigramTokenize <- function(x) NGramTokenizer(x, Weka_control(min = 2, max = 2))
+trigramTokenize <- function(x) NGramTokenizer(x, Weka_control(min = 3, max = 3))
+tdm2 <- TermDocumentMatrix(corp, control = list(tokenize = bigramTokenize))
+tdm3 <- TermDocumentMatrix(corp, control = list(tokenize = trigramTokenize))
+
